c770767c1d9283d0cbc27e1206731708732fe8fd
Imran Fakhrul <fakhrul_imran@yahoo.com>
2020-05-06 13:18:09 +0600

    ADD: Add post, delete route support

--- Router.php
+++ Router.php
@@ -1,37 +1,69 @@
 <?php
+
 namespace Routing;
 
-class Router {
+class Router
+{
     private static $nomatch = true;
 
-    public static function get($pattern, $callback) {
+    public static function get($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+            return;
+        }
+
+        self::process($pattern, $callback);
+    }
+
+    public static function post($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+            return;
+        }
+
+        self::process($pattern, $callback);
+    }
+
+    public static function delete($pattern, $callback)
+    {
+        if ($_SERVER['REQUEST_METHOD'] != 'DELETE') {
+            return;
+        }
+
+        self::process($pattern, $callback);
+    }
+
+    private static function process($pattern, $callback)
+    {
         $pattern = "~^{$pattern}/?$~";
-        $url = self::getUrl();
-        
+
         $params = self::getMatches($pattern);
 
-        if($params && is_callable($callback)) {
+        if ($params && is_callable($callback)) {
             self::$nomatch = false;
-            
+
             $functionArguments = array_slice($params, 1);
+
             $callback(...$functionArguments);
         }
     }
 
-    public static function cleanup() {
-        if(self::$nomatch) {
+    public static function cleanup()
+    {
+        if (self::$nomatch) {
             echo "No route matched!";
         }
     }
 
-    private static function getUrl() {
+    private static function getUrl()
+    {
         return $_SERVER['REQUEST_URI'];
     }
 
-    private static function getMatches($pattern) {
+    private static function getMatches($pattern)
+    {
         $url = self::getUrl();
 
-         return preg_match($pattern, $url, $matches) ? $matches : false;
-        
+        return preg_match($pattern, $url, $matches) ? $matches : false;
     }
 }
--- routes.php
+++ routes.php
@@ -3,12 +3,32 @@ require "Router.php";
 
 use Routing\Router;
 
-Router::get('/', function() {
+Router::get('/', function () {
     echo "Welcome home!";
 });
 
-Router::get('/hello', function() {
+Router::get('/hello', function () {
     echo "Hello there!";
+});
+
+Router::get('/greet/(\w+)', function ($name) {
+    echo "Hello {$name}!";
+});
+
+Router::get('/greet/(\w+)/title/(\w+)', function ($name, $title) {
+    echo "Hello {$title} {$name}!";
+});
+
+Router::get('/verb', function () {
+    echo $_SERVER['REQUEST_METHOD'];
+});
+
+Router::post('/verb', function () {
+    echo $_SERVER['REQUEST_METHOD'];
+});
+
+Router::delete('/verb', function () {
+    echo $_SERVER['REQUEST_METHOD'];
 });
 
 // For no route match
